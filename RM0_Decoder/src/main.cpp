d91502c0f5d5363f9f3e79a371c0123940a2d511
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2021-10-19 13:31:47 +0200

    chore(Main): prepare C++ projects

---
+++ RM0_Decoder/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "I'm the MPEG Haptics RM0 Decoder";
+    return 0;
+}
---
+++ RM0_Encoder/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!";
+    std::cout << "I'm the MPEG Haptics RM0 Encoder";
+    return 0;
+}
