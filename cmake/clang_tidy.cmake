fd21b5298239405608191942f5b6e9e17d31757a
Fabien Danieau <fabien.danieau@interdigital.com>
2021-11-08 10:08:04 +0100

    test(#3 - clang tidy): adding cmake file for clang-tidy and call from main CMakeList

---
+++ cmake/clang_tidy.cmake
@@ -0,0 +1,18 @@
+#from http://mpegx.int-evry.fr/software/MPEG/MIV/RS/TM1/-/blob/main/cmake/clang_tidy.cmake
+
+cmake_minimum_required(VERSION 3.13 FATAL_ERROR)
+
+option(ENABLE_CLANG_TIDY "Turn on clang_tidy processing if available" ON)
+
+if (ENABLE_CLANG_TIDY)
+    find_program(CLANG_TIDY_PATH NAMES "clang-tidy")
+
+    if(CLANG_TIDY_PATH)
+        set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_PATH}")
+        message(STATUS "The clang-tidy path is set to ${CLANG_TIDY_PATH}")
+    else()
+        message(STATUS "clang-tidy could not be found")
+    endif()
+else()
+    message(STATUS "clang-tidy is disabled")
+endif()
