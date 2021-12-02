5365c5b34e548df4d38d2d59ef4661ab9229f063
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2021-12-02 18:11:45 +0100

    feat(JSONParsing): add nlohmann json library in the encoder project

--- source/Encoder/include/AhapEncoder.h
+++ source/Encoder/include/AhapEncoder.h
@@ -31,43 +31,16 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <Tools/include/InputParser.h>
-#include <../include/AhapEncoder.h>
+#ifndef _AHAPENCODER_H_
+#define _AHAPENCODER_H_
 
-using haptics::encoder::AhapEncoder;
-using haptics::tools::InputParser;
+#include <iostream>
 
-// NOLINTNEXTLINE(bugprone-exception-escape)
-auto main(int argc, char *argv[]) -> int {
-  const auto args = std::vector<const char *>(argv, argv + argc);
-  InputParser inputParser(args);
-  if (inputParser.cmdOptionExists("-h") || inputParser.cmdOptionExists("--help")) {
-    InputParser::help(args[0]);
-    return EXIT_SUCCESS;
-  }
+namespace haptics::encoder {
 
-  std::string filename = inputParser.getCmdOption("-f");
-  if (filename.empty()) {
-    filename = inputParser.getCmdOption("--file");
-  }
-  if (filename.empty()) {
-    std::cout << "The file to process is : " << filename << "\n";
-    InputParser::help(args[0]);
-    return EXIT_FAILURE;
-  }
-
-  std::string output = inputParser.getCmdOption("-o");
-  if (output.empty()) {
-    output = inputParser.getCmdOption("--output");
-  }
-  if (!output.empty()) {
-    std::cout << "The generated file will be : " << output << "\n";
-  }
-
-  std::string ext = InputParser::getFileExt(filename);
-  if (ext == "json" || ext == "ahap") {
-    std::cout << "The AHAP file to encode : " << filename << std::endl;
-    AhapEncoder::encode(filename);
-  }
-  return EXIT_SUCCESS;
-}
+class AhapEncoder {
+public:
+  [[nodiscard]] auto static AhapEncoder::encode(std::string& filename) -> int;
+};
+} // namespace haptics::encoder
+#endif //_AHAPENCODER_H_
