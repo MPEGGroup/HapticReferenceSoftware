76354921549fd5508e8bb6996904a93dedf2a604
Fabien Danieau <fabien.danieau@interdigital.com>
2021-12-15 22:26:36 +0100

    fix(#14): setting dr_wav to version 0.13.4

--- cmake/dr_libs.cmake
+++ cmake/dr_libs.cmake
@@ -5,13 +5,13 @@ option(BUILD_DR_LIBS "Build and use DR_LIBS" ON)
 if(BUILD_DR_LIBS)
     include(FetchContent)
     if(NO_INTERNET)
-        set(LOCAL_DR_LIBS_DIR ${CMAKE_SOURCE_DIR}/../dr_libs CACHE PATH "Path to the local dr_libs directory" )
+        set(LOCAL_DR_LIBS_DIR ${CMAKE_SOURCE_DIR}/../dr_libs-0.13.4 CACHE PATH "Path to the local dr_libs directory" )
         message(STATUS "Looking for a local copy of the DR_LIBS test framework in ${LOCAL_DR_LIBS_DIR}")
         fetchcontent_declare(DR_LIBS URL ${LOCAL_DR_LIBS_DIR})
     else()
         fetchcontent_declare(DR_LIBS
                 GIT_REPOSITORY https://github.com/mackron/dr_libs.git
-                #GIT_TAG "1.9.0"
+                GIT_TAG "49de65c5204cb055afc1a14b11753e17d52a1d1b"
                 GIT_PROGRESS TRUE
                 )
     endif()
