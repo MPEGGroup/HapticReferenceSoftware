4e530fed2ad5778a44ad0dc91da1b7cae974d7aa
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2021-12-17 12:30:11 +0100

    feat(#13 XML): add XML library to parse IVS files

--- source/Encoder/src/IvsEncoder.cpp
+++ source/Encoder/src/IvsEncoder.cpp
@@ -31,48 +31,22 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <Tools/include/InputParser.h>
-#include <Encoder/include/AhapEncoder.h>
 #include <Encoder/include/IvsEncoder.h>
 
-using haptics::encoder::AhapEncoder;
-using haptics::encoder::IvsEncoder;
-using haptics::tools::InputParser;
+namespace haptics::encoder {
 
-// NOLINTNEXTLINE(bugprone-exception-escape)
-auto main(int argc, char *argv[]) -> int {
-    const auto args = std::vector<const char *>(argv, argv + argc);
-    InputParser inputParser(args);
-    if (inputParser.cmdOptionExists("-h") || inputParser.cmdOptionExists("--help")) {
-        InputParser::help(args[0]);
-        return EXIT_SUCCESS;
-    }
-
-    std::string filename = inputParser.getCmdOption("-f");
+[[nodiscard]] auto IvsEncoder::encode(std::string& filename) -> int {
     if (filename.empty()) {
-        filename = inputParser.getCmdOption("--file");
-    }
-    if (filename.empty()) {
-        std::cout << "The file to process is : " << filename << "\n";
-        InputParser::help(args[0]);
         return EXIT_FAILURE;
     }
 
-    std::string output = inputParser.getCmdOption("-o");
-    if (output.empty()) {
-        output = inputParser.getCmdOption("--output");
-    }
-    if (!output.empty()) {
-        std::cout << "The generated file will be : " << output << "\n";
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(filename.c_str());
+    if (!result) {
+        return EXIT_FAILURE;
     }
 
-    std::string ext = InputParser::getFileExt(filename);
-    if (ext == "json" || ext == "ahap") {
-        std::cout << "The AHAP file to encode : " << filename << std::endl;
-        AhapEncoder::encode(filename);
-    } else if (ext == "xml" || ext == "ivs") {
-        std::cout << "The IVS file to encode : " << filename << std::endl;
-        IvsEncoder::encode(filename);
-    }
     return EXIT_SUCCESS;
 }
+
+} // namespace haptics::encoder
