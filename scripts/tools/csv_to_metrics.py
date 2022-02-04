c04a4b0b532094c094c2fb68f50800e9be9a959d
Fabien Danieau <fabien.danieau@interdigital.com>
2022-02-04 17:32:25 +0100

    test(#62): displaying metrics in merge request

--- scripts/tools/csv_to_metrics.py
+++ scripts/tools/csv_to_metrics.py
@@ -5,7 +5,7 @@
 # and contributor rights, including patent rights, and no such rights are
 # granted under this license.
 #
-# Copyright (c) 2010-2021, ISO/IEC
+# Copyright (c) 2010-2022, ISO/IEC
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,31 +32,26 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 
-from soundfile import read
+import csv
 import argparse
-import sys
 from os import path
-
-
-def compute_bitrate(wav_file, compressed_file):
-    data_original, sample_rate_original = read(wav_file)
-    duration_s = len(data_original) / sample_rate_original
-    with open(compressed_file, 'rb') as f:
-        bit_size = len(f.read())
-    bit_rate = bit_size / duration_s / 1000.0
-    return bit_rate
+import sys
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Computing Bitrate')
-    parser.add_argument("original", help="path to the original file")
-    parser.add_argument("compressed", help="path to the compressed file")
+    parser = argparse.ArgumentParser(description='Converting CSV to OpenMetrics')
+    parser.add_argument("csv_file", help="path to the csv file")
+    parser.add_argument("metrics_file", help="path to the metrics file")
 
     args = parser.parse_args()
-    if(not path.exists(args.original)):
-        sys.exit(args.original + " file could not be found")
-    if(not path.exists(args.compressed)):
-        sys.exit(args.compressed + " file could not be found")
+    if(not path.exists(args.csv_file)):
+        sys.exit(args.csv_file + " file could not be found")
 
-    bitrate = compute_bitrate(args.original, args.compressed)
-    print("The bitrate is:", bitrate, " Kbps")
+    f = open(args.metrics_file, "w")
+    with open(args.csv_file, 'r', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        next(spamreader)  # skip header
+        for row in spamreader:
+            name = path.basename(row[9])
+            f.write(name+" "+row[10]+"\n")
+    f.close()
