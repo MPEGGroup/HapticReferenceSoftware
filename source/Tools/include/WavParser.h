f936c5c4a452c3e189ca941e970315be5ae65039
Quentin Galvane <Quentin.Galvane@InterDigital.com>
2022-02-07 21:05:04 +0100

    fix (#58): cleaning wavParser

--- source/Tools/include/WavParser.h
+++ source/Tools/include/WavParser.h
@@ -47,8 +47,8 @@ constexpr uint16_t NEG_MAX = 32768; // 2^15
 class WavParser {
 public:
   auto loadFile(const std::string &filename) -> bool;
-  static auto saveFile(std::string &filename, std::vector<double> &buff, int sampleRate) -> bool;
-  static auto saveFile(std::string &filename, std::vector<std::vector<double>> &buff,
+  static auto saveFile(const std::string &filename, const std::vector<double> &buff, int sampleRate) -> bool;
+  static auto saveFile(const std::string &filename, const std::vector<std::vector<double>> &buff,
                        int sampleRate) -> bool;
   [[nodiscard]] auto getSamplerate() const -> uint32_t;
   [[nodiscard]] auto getNumChannels() const -> size_t;
