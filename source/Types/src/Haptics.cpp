50a9b3e2079fd6e6f091ede29ae56cfed08d2401
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2022-02-17 17:43:21 +0100

    fix(#66): use linear interpolation for kinesthetic files

--- source/Types/src/Haptics.cpp
+++ source/Types/src/Haptics.cpp
@@ -79,7 +79,8 @@ auto Haptics::loadMetadataFromOHM(haptics::tools::OHMData data) -> void {
   for (int i = 0; i < numElements; i++) {
     auto element = data.getHapticElementMetadataAt(i);
     std::string elemDescription = element.elementDescription;
-    Perception perception(i, 0, elemDescription, PerceptionModality::Other);
+    PerceptionModality perceptionModality = Perception::convertToModality(elemDescription);
+    Perception perception(i, 0, elemDescription, perceptionModality);
     short numChannels = element.numHapticChannels;
     for (int j = 0; j < numChannels; j++) {
       auto channel = element.channelsMetadata[j];
