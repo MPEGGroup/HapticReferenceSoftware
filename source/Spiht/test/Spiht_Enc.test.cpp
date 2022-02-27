c5f972d85edbadad90d80317a36488ec4d269737
LarsNockenberg <Lars.Nockenberg@mytum.de>
2022-02-27 21:55:35 +0100

    fix(#73): cleanup

--- source/Spiht/test/Spiht_Enc.test.cpp
+++ source/Spiht/test/Spiht_Enc.test.cpp
@@ -123,7 +123,7 @@ TEST_CASE("haptics::spiht::Spiht_Enc,2") {
       Keyframe keyframe(i, (float)(i % MOD_VAL) / MOD_VAL, 0);
       effect_in.addKeyframe(keyframe);
     }
-    Keyframe keyframe(bl, scalar, 0); // NOLINT
+    Keyframe keyframe(bl, scalar, 0);
     effect_in.addKeyframe(keyframe);
     Keyframe keyframeBits(bl + 1, (float)BITS_EFFECT, 0);
     effect_in.addKeyframe(keyframeBits);
