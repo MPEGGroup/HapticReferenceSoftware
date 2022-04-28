26e985302e7ddd1375564703833b0d5204738a9f
Alexandre Hulsken <alexandre.hulsken@interhaptics.com>
2022-04-28 10:43:23 +0200

    feat(#82): compatibility of added metadata in the interchange format

--- source/Types/include/Track.h
+++ source/Types/include/Track.h
@@ -47,6 +47,10 @@ struct Direction {
 
   explicit Direction() = default;
   explicit Direction(int8_t x, int8_t y, int8_t z) : X(x), Y(y), Z(z){};
+
+  auto operator==(const Direction &other) const -> int {
+    return X == other.X && Y == other.Y && Z == other.Z;
+  };
 };
 
 class Track {
