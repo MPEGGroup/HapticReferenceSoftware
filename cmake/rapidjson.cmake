dd8e4273aa86cfcff5d2947c4d5246547647bb53
Henry Da Costa <hdacosta@immersion.com>
2022-09-15 14:40:01 -0400

    fix #112: freezing RapidJSON commit to protect against future instanbilities

--- cmake/rapidjson.cmake
+++ cmake/rapidjson.cmake
@@ -11,7 +11,7 @@ else()
         GIT_REPOSITORY https://github.com/Tencent/rapidjson.git
         GIT_PROGRESS TRUE
         GIT_SHALLOW TRUE
-        GIT_TAG master
+        GIT_TAG 06d58b9e848c650114556a23294d0b6440078c61
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND "")
 endif()
