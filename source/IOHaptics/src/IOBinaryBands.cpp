98f9d860f1794d342454c169152fc901bf6c3777
Lars Nockenberg <lars.nockenberg@tum.de>
2022-10-10 12:17:58 +0200

    clang format

--- source/IOHaptics/src/IOBinaryBands.cpp
+++ source/IOHaptics/src/IOBinaryBands.cpp
@@ -78,7 +78,7 @@ auto IOBinaryBands::writeBandHeader(types::Band &band, std::ostream &file) -> bo
     IOBinaryPrimitives::writeNBytes<uint8_t, 1>(curveType, file);
   } else if (band.getBandType() == types::BandType::WaveletWave) {
     auto bl_ms = band.getBlockLength();
-    auto blockLength_samples = (int)(bl_ms/S_2_MS_WAVELET * band.getUpperFrequencyLimit());
+    auto blockLength_samples = (int)(bl_ms / S_2_MS_WAVELET * band.getUpperFrequencyLimit());
     IOBinaryPrimitives::writeNBytes<uint16_t, 2>(blockLength_samples, file);
   }
 
