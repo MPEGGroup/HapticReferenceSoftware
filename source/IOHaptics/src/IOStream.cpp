38e175e3de853dbcc0ae337c3a500c042c84f7ee
Quentin Galvane <Quentin.Galvane@InterDigital.com>
2023-08-18 11:23:27 +0200

    Fix readEffect call

--- source/IOHaptics/src/IOStream.cpp
+++ source/IOHaptics/src/IOStream.cpp
@@ -2570,7 +2570,7 @@ auto IOStream::readListObject(std::vector<bool> &bitstream, int fxCount, types::
   for (int i = 0; i < fxCount; i++) {
     std::vector<bool> fxBits(bitstream.begin() + idx, bitstream.end());
     types::Effect effect;
-    if (!readEffect(fxBits, effect, band, idx, timescale)) {
+    if (!readEffect(fxBits, effect, band, idx)) {
       return false;
     }
     fxList.push_back(effect);
