764dcc3422a85dc2aa0b2876e9587d1f921c8d7c
Lars Nockenberg <lars.nockenberg@tum.de>
2023-12-22 13:24:53 +0100

    catch2

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -5,13 +5,13 @@ option(BUILD_CATCH2 "Build and use Catch2 for unit tests" ON)
 if(BUILD_CATCH2)
     include(FetchContent)
     if(NO_INTERNET)
-        set(LOCAL_CATCH2_DIR ${CMAKE_SOURCE_DIR}/../Catch2-2.13.10 CACHE PATH "Path to the local Catch2 directory" )
+        set(LOCAL_CATCH2_DIR ${CMAKE_SOURCE_DIR}/../Catch2-2.13.4 CACHE PATH "Path to the local Catch2 directory" )
         message(STATUS "Looking for a local copy of the Catch2 test framework in ${LOCAL_CATCH2_DIR}")
         fetchcontent_declare(CATCH2 URL ${LOCAL_CATCH2_DIR})
     else()
         fetchcontent_declare(CATCH2
                 GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-                GIT_TAG "v2.13.10"
+                GIT_TAG "v2.13.4"
                 GIT_PROGRESS TRUE
                 GIT_SHALLOW ON
                 )
