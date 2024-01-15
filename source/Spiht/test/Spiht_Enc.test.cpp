75b3ef61a1f7029c3244918607de25d645a04e0b
Lars Nockenberg <ga45rij@mytum.de>
2024-01-15 14:34:20 +0100

    Fix Spiht test

--- source/Spiht/test/Spiht_Enc.test.cpp
+++ source/Spiht/test/Spiht_Enc.test.cpp
@@ -180,7 +180,7 @@ TEST_CASE("haptics::spiht::Spiht_Enc,2") {
     Spiht_Enc enc;
     std::vector<int> in;
     for (size_t i = 0; i < bl; i++) {
-      in.push_back((int)((float)(i % MOD_VAL) / MOD_VAL));
+      in.push_back(i % MOD_VAL);
     }
     std::vector<unsigned char> stream_enc;
     enc.encodeEffect(in, BITS_EFFECT, scalar, stream_enc);
