13869dca34c73799bca622a8e2703fe0bbca7897
Lars Nockenberg <lars.nockenberg@tum.de>
2026-06-01 15:23:31 +0200

    fix clang format

--- source/Encoder/src/main.cpp
+++ source/Encoder/src/main.cpp
@@ -122,7 +122,8 @@ auto isValidBlocklength(std::optional<int> &blocklength, InputParser &inputParse
   if (inputParser.cmdOptionExists("--packet_duration")) {
     packetDuration = std::stoi(inputParser.getCmdOption("--packet_duration"));
   }
-  return !(((double)blocklength_chosen / (double)DEFAULT_FS) > ((double)packetDuration / (double)S2MS));
+  return !(((double)blocklength_chosen / (double)DEFAULT_FS) > 
+           ((double)packetDuration / (double)S2MS));
 }
 
 auto generateEncodingConfig(std::optional<int> bitrate, std::optional<int> budget,
